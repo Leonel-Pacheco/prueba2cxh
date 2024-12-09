b148a43de0cefe319ca2427dbfafcf9a2cb7871b
Leoo2002 <leonelquirogapacheco@gmail.com>
2024-12-09 17:04:08 -0300

    sumamos componentes y 1 servicio

---
+++ src/app/astudillo/astudillo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-astudillo',
+  imports: [],
+  templateUrl: './astudillo.component.html',
+  styleUrl: './astudillo.component.css'
+})
+export class AstudilloComponent {
+
+}
